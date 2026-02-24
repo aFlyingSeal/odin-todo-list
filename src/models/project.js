03212ea6598be6201bf80d5c09217885ad4cd672
Dat Le <thanhdat952006@gmail.com>
2026-02-24 21:43:31 +0700

    Moved project.js and task.js to models for clarity

---
+++ src/models/project.js
@@ -0,0 +1,33 @@
+import { getState } from "../logic/state";
+import { saveState } from "../logic/storage";
+
+class Project{
+    constructor(title, description){
+        this.title = title;
+        this.description = description;
+
+        this.tasks = [];
+        this.id = crypto.randomUUID();
+    }
+
+    addTask(task){
+        this.tasks.push(task);
+
+        saveState(getState());
+    }
+    removeTask(id){
+        this.tasks = this.tasks.filter(task => task.getId() !== id);
+
+        saveState(getState());
+    }
+    getTaskById(id){
+        return this.tasks.find(task => task.getId() === id) || null;
+    }
+    filterByPriority(priority){
+        return this.tasks.filter(task => task.getPriority() === priority);
+    }
+
+    getId(){ return this.id; }
+};
+
+export default Project;
