3773b3f2adcd80d8548bba45701e1ad1a86a0b79
Dat Le <thanhdat952006@gmail.com>
2026-02-25 21:21:50 +0700

    Hides form on startup, initiated required UI elements for project to work

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import Task from "./logic/task";
-import Project from "./logic/project";
-import { addProject, removeProject, setActiveProject,
-         getActiveProject, getProjects, getState,
-         hydrateState } from "./logic/state";
-import { loadState } from "./logic/storage";
+import { hydrateState, getActiveProject } from "./logic/state";
+import { clearState, loadState } from "./logic/storage";
+import { initNavBar, initProjectForm } from "./ui/nav-bar";
+import { initTaskForm } from "./ui/task-form";
 
 // on startup, load saved state
 const saved = loadState();
@@ -11,23 +9,12 @@ if (saved){
     hydrateState(saved);
 }
 
-// sample test case
-const project1 = new Project("Project 1", "Project desc 1");
-const task1 = new Task("Task 1", "Desc 1", Date.now(), "A");
-const task2 = new Task("Task 2", "Desc 2", Date.now(), "B");
+const projectModal = document.querySelector(".modal-project");
+const taskModal = document.querySelector(".modal-task");
 
-project1.addTask(task1);
-project1.addTask(task2);
+projectModal.style.display = 'none';
+taskModal.style.display = 'none';
 
-const project2 = new Project("Project 2", "Project desc 2");
-const task3 = new Task("Task 3", "Desc 3", Date.now(), "B");
-const task4 = new Task("Task 4", "Desc 4", Date.now(), "A");
-
-project2.addTask(task3);
-project2.addTask(task4);
-
-addProject(project1);
-addProject(project2);
-
-// debugging log to check if storage.js is working
-console.log(getState());
+initNavBar();
+initProjectForm();
+initTaskForm();
